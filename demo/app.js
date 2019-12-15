007df8cc8a3d6b127787e559eaab3b3e54910ef3
Natalia Svistunova <big.plavnik@gmail.com>
2019-12-15 18:45:55 +0100

    demo

--- demo/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@latest/dist/vue.esm.browser.min.js'
-Vue.use(httpVueLoader)
-
-new Vue({
-  el: '#app',
-  components: {
-  	'popup-widget': httpVueLoader('./src/PopupWidget.vue')
-  }
-})
