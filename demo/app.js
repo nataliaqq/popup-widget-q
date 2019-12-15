71816420155b2b8f2fd0168e2b21b6d0a3f6ecb5
Natalia Svistunova <big.plavnik@gmail.com>
2019-12-15 18:38:54 +0100

    demo

--- demo/app.js
+++ demo/app.js
@@ -1,5 +1,4 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@latest/dist/vue.esm.browser.min.js'
-import httpVueLoader from 'https://unpkg.com/http-vue-loader'
 Vue.use(httpVueLoader)
 
 new Vue({
